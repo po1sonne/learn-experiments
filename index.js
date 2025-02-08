3bce1f4fa07deaa55b0e453cb237724bbd3f1a3b
po1sonne <hyper1on@outlook.com>
2025-02-09 01:59:56 +0300

    update

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-let x = 1
-console.log(x)
+let firstName = prompt("Enter your name:");
+let age = parseInt(prompt("Enter your age:"));
+alert("Привет, " + firstName + " тебе " + age + " лет! Пипец ты старый. Ну всё, пока!");
